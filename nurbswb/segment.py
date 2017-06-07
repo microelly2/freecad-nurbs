e043d91dcde88b858e04a3bed7406e0d0b7030c4
microelly2 <thomas@freecadbuch.de>
2017-06-07 16:21:52 +0200

    finesegment for closed nurbs

--- nurbswb/segment.py
+++ nurbswb/segment.py
@@ -14,6 +14,8 @@ App = FreeCAD
 Gui = FreeCADGui
 
 import Part
+import numpy as np
+
 
 
 class PartFeature:
@@ -21,8 +23,6 @@ class PartFeature:
 	def __init__(self, obj):
 		obj.Proxy = self
 		self.Object = obj
-
-# grundmethoden zum sichern
 
 	def attach(self, vobj):
 		self.Object = vobj.Object
@@ -52,24 +52,29 @@ class ViewProvider:
 
 
 class Segment(PartFeature):
-	'''parametric filled face'''
+	'''segment einer bspline  flaeche oder kurve
+	Einschraenkung:
+	es wird die erste flaeche Face1 bzw. die erste Kante Edge1 verarbeitet
+	'''
 
 	def __init__(self, obj):
 		PartFeature.__init__(self, obj)
 
 		obj.addProperty("App::PropertyLink", "source", "Base")
-
 		obj.addProperty("App::PropertyInteger", "umin", "Base")
 		obj.addProperty("App::PropertyInteger", "umax", "Base")
 		obj.addProperty("App::PropertyInteger", "vmin", "Base")
 		obj.addProperty("App::PropertyInteger", "vmax", "Base")
+		obj.addProperty("App::PropertyBool", "closeV", "Base")
 
 		obj.umax=-1
 		obj.vmax=-1
+
 		ViewProvider(obj.ViewObject)
 
 
 	def execute(proxy, obj):
+
 		if  len(obj.source.Shape.Faces) >= 1:
 			face=obj.source.Shape.Face1
 			bs=face.Surface.copy()
@@ -81,6 +86,9 @@ class Segment(PartFeature):
 			bs=edge.Curve.copy()
 			ks=bs.getKnots()
 			bs.segment(ks[obj.umin],ks[obj.umax])
+
+		if obj.closeV:
+			bs.setVPeriodic()
 
 		obj.Shape=bs.toShape()
 
@@ -94,13 +102,12 @@ def createSegment(name="MySegment"):
 
 
 class NurbsTrafo(PartFeature):
-	'''parametric filled face'''
+	'''rotieren des pole-array, um naht zu verschieben'''
 
 	def __init__(self, obj):
 		PartFeature.__init__(self, obj)
 
 		obj.addProperty("App::PropertyLink", "source", "Base")
-
 		obj.addProperty("App::PropertyInteger", "start", "Base")
 		obj.addProperty("App::PropertyInteger", "umax", "Base")
 		obj.addProperty("App::PropertyInteger", "vmin", "Base")
@@ -112,20 +119,47 @@ class NurbsTrafo(PartFeature):
 
 
 	def execute(proxy, obj):
-		bc=obj.source.Shape.Edge1.Curve.copy()
-		pols=bc.getPoles()
-		i=obj.start
-		pols2=pols[i:] +pols[:i]
+		if  len(obj.source.Shape.Faces) >= 1:
+			face=obj.source.Shape.Face1
+			bs=face.Surface.copy()
 
-		multies=bc.getMultiplicities()
-		knots=bc.getKnots()
-		deg=bc.Degree
-		bc.buildFromPolesMultsKnots(pols2,multies,knots,True,deg)
+			poles=bs.getPoles()
+			ku=bs.getUKnots()
+			kv=bs.getVKnots()
+			mu=bs.getUMultiplicities()
+			mv=bs.getVMultiplicities()
 
-		obj.Shape=bc.toShape()
+			y=np.array(poles).swapaxes(0,1)
+			k=obj.start
+
+			poles2=np.concatenate([y[k:],y[:k]]).swapaxes(0,1)
+
+			bs2=Part.BSplineSurface()
+			bs2.buildFromPolesMultsKnots(poles2,
+				mu,mv,
+				ku,kv,
+				False,True,3,3,)
+
+			obj.Shape=bs2.toShape()
+
+		else:
+			bc=obj.source.Shape.Edge1.Curve.copy()
+			pols=bc.getPoles()
+
+			multies=bc.getMultiplicities()
+			knots=bc.getKnots()
+			deg=bc.Degree
+
+			i=obj.start
+			pols2=pols[i:] +pols[:i]
+			bc.buildFromPolesMultsKnots(pols2,multies,knots,True,deg)
+
+			obj.Shape=bc.toShape()
+
 
 
 def createNurbsTrafo(name="MyNurbsTafo"):
+	''' erzeugt ein NurbsTrafo Objekt '''
 
 	ffobj = FreeCAD.activeDocument().addObject(
 		"Part::FeaturePython", name)
@@ -135,14 +169,16 @@ def createNurbsTrafo(name="MyNurbsTafo"):
 
 
 class FineSegment(PartFeature):
+	''' erzeugt ein feines Segment, dass feienr ist als die normale Segmentierung des nurbs
+	factor gibt die Anzahl der Abstufungen an
+	die Zahlen umin, ... vmax sind ganzzahlige Anteile von factor
+	'''
 
 	def __init__(self, obj):
 		PartFeature.__init__(self, obj)
 
 		obj.addProperty("App::PropertyLink", "source", "Base")
-
 		obj.addProperty("App::PropertyInteger", "factor", "Base")
-
 		obj.addProperty("App::PropertyInteger", "umin", "Base")
 		obj.addProperty("App::PropertyInteger", "umax", "Base")
 		obj.addProperty("App::PropertyInteger", "vmin", "Base")
@@ -163,8 +199,11 @@ class FineSegment(PartFeature):
 
 	def onChanged(self, obj, prop):
 		if prop in ["vmin","vmax","umin","umax","source"]:
+			
 			face=obj.source.Shape.Face1
 			bs=face.Surface.copy()
+#			bs.setUNotPeriodic()
+#			bs.setVNotPeriodic()
 
 			if obj.umin<0: obj.umin=0
 			if obj.vmin<0: obj.vmin=0
@@ -179,17 +218,30 @@ class FineSegment(PartFeature):
 			vmin=1.0/obj.factor*obj.vmin
 			vmax=1.0/obj.factor*obj.vmax
 
+			if bs.isVPeriodic() and not vmax< bs.getVKnots()[-1]:
+				vmax=bs.getVKnots()[-1]
+# geht so nicht: 
+#				obj.vmax=int(round(vmax*obj.factor,0))
 
-			if umin>0:
+			if bs.isUPeriodic() and not umax< bs.getUKnots()[-1]:
+				umax=bs.getUKnots()[-1]
+#				obj.umax=int(round(umax*obj.factor,0))
+
+
+			print  bs.getUKnots()
+			print  bs.getVKnots()
+			print  [umin,umax,vmin,vmax]
+
+			if umin>0 and umin not in bs.getUKnots():
 				bs.insertUKnot(umin,1,0)
 
-			if umax<obj.factor:
+			if umax<obj.factor and umax not in bs.getUKnots():
 				bs.insertUKnot(umax,1,0)
 
-			if vmin>0:
+			if vmin>0 and vmin not in bs.getVKnots():
 				bs.insertVKnot(vmin,1,0)
 
-			if vmax<obj.factor:
+			if vmax<obj.factor and vmax not in bs.getVKnots(): # and vmax< bs.getVKnots()[-1]:
 				bs.insertVKnot(vmax,1,0)
 
 
@@ -197,7 +249,9 @@ class FineSegment(PartFeature):
 			obj.Shape=bs.toShape()
 
 
+
 def createFineSegment(name="MyFineSegment"):
+	''' erzeugt ein FineSegment Objekt '''
 
 	ffobj = FreeCAD.activeDocument().addObject(
 		"Part::FeaturePython", name)
@@ -206,14 +260,13 @@ def createFineSegment(name="MyFineSegment"):
 
 
 
-
 def run():
+	'''anwendungsfall fuer die selection wird ein segment erzeugt'''
 
 	source=None
 	if len( Gui.Selection.getSelection())<>0:
 		source=Gui.Selection.getSelection()[0]
 	s=createSegment()
-	
 	s.source=source
 	sm.umax=-2
 	sm.umin=2
