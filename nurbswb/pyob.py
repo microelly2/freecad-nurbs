62725320eabfa8474f2b796078955fefac204936
microelly2 <thomas@freecadbuch.de>
2017-06-23 14:28:49 +0200

    python object base class added

---
+++ nurbswb/pyob.py
@@ -0,0 +1,50 @@
+'''python objects for freecad'''
+
+# -*- coding: utf-8 -*-
+#-- microelly 2017 v 0.1
+#-- GNU Lesser General Public License (LGPL)
+
+
+##\cond
+import FreeCAD
+import FreeCADGui
+App = FreeCAD
+Gui = FreeCADGui
+
+import Part
+import numpy as np
+
+
+class PartFeature:
+	''' basic defs'''
+
+	def __init__(self, obj):
+		obj.Proxy = self
+		self.Object = obj
+
+	def attach(self, vobj):
+		self.Object = vobj.Object
+
+	def claimChildren(self):
+		return self.Object.Group
+
+	def __getstate__(self):
+		return None
+
+	def __setstate__(self, state):
+		return None
+
+
+class ViewProvider:
+	''' basic defs '''
+
+	def __init__(self, obj):
+		obj.Proxy = self
+		self.Object = obj
+
+	def __getstate__(self):
+		return None
+
+	def __setstate__(self, state):
+		return None
+##\endcond
