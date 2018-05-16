8ca9b91a99c930598def66b79e6719e71b36c59b
microelly2 <thomas@freecadbuch.de>
2018-05-16 22:21:42 +0200

    some extra methods

--- nurbswb/sketchmanager.py
+++ nurbswb/sketchmanager.py
@@ -6,10 +6,6 @@ import nurbswb.pyob
 import time
 import glob
 
-from PySide import QtGui, QtCore
-
-
-
 
 import FreeCAD,FreeCADGui
 App=FreeCAD
@@ -17,6 +13,15 @@ Gui=FreeCADGui
 
 import PySide
 from PySide import  QtGui,QtCore
+
+
+
+def getNamedConstraint(sketch,name):
+	'''get the index of a constraint name'''
+	for i,c in enumerate (sketch.Constraints):
+		if c.Name==name: return i
+	print ('Constraint name "'+name+'" not in ' +sketch.Label)
+	raise Exception ('Constraint name "'+name+'" not in ' + sketch.Label)
 
 
 def run(w):
@@ -29,6 +34,43 @@ def run(w):
 	print sk.Label
 	print sk.Name
 
+def hideAllConstraints(w,show=False):
+	sk=w.obj.Object
+	c=App.ActiveDocument.ActiveObject.Constraints
+	for i in range(len(c)):
+		sk.setVirtualSpace(i, not show)
+
+def showEndPoints(w,show=False):
+	sk=w.obj.Object
+	cs=[getNamedConstraint(sk,l) for l in ['p_0_x','p_0_y','p_1_x','p_1_y']]
+	for i in cs:
+		sk.setVirtualSpace(i, not show)
+
+def showArcs(w,show=False):
+	sk=w.obj.Object
+	for l in ['tangent_AB','tangent_BC','tangent_CD']:
+		try:
+			cs=getNamedConstraint(sk,l) 
+			sk.setVirtualSpace(cs, not show)
+		except: pass
+
+import Sketcher
+def runSelection(w,mode=None):
+		sk=w.obj.Object
+		s=Gui.Selection.getSelectionEx()[0]
+		for el in s.SubElementNames:
+			el=int(el.replace("Edge",""))
+			try:
+				cs=getNamedConstraint(sk,'block edge ' + str(el)) 
+				sk.delConstraint(cs)
+			except:
+				c=sk.addConstraint(Sketcher.Constraint('Block',el-1)) 
+				sk.renameConstraint(c, u'block edge ' + str(el) )
+				if sk.solve()<>0:
+					print "kann block nicht ausfuehren"
+					sk.delConstraint(c)
+					sk.solve()
+
 
 def dialog(obj):
 
@@ -39,16 +81,100 @@ def dialog(obj):
 	w.setLayout(box)
 	w.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
-	l=QtGui.QLabel("Anzahl" )
-	w.l=l
-	box.addWidget(l)
+	w.l=QtGui.QLabel("Sketcher Dialog Extension" )
+	box.addWidget(w.l)
 
 	w.r=QtGui.QPushButton("run")
 	box.addWidget(w.r)
 	w.r.pressed.connect(lambda :run(w))
 
+	w.r=QtGui.QPushButton("hide All Constraints")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :hideAllConstraints(w,False))
+
+	w.r=QtGui.QPushButton("show All Constraints")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :hideAllConstraints(w,True))
+
+	w.r=QtGui.QPushButton("show Endpoint coordinates")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :showEndPoints(w,True))
+
+	w.r=QtGui.QPushButton("hide Endpoint coordinates")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :showEndPoints(w,False))
+
+	w.r=QtGui.QPushButton("show Arc dimensions")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :showArcs(w,True))
+
+	w.r=QtGui.QPushButton("hide Arc dimensions")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :showArcs(w,False))
+
+	w.r=QtGui.QPushButton("block or unblock selections")
+	box.addWidget(w.r)
+	w.r.pressed.connect(lambda :runSelection(w,None))
+
+
+	w=ComboViewShowWidget(w)
+
+	box.addItem(QtGui.QSpacerItem(
+			10, 10, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding))
+
 
 	return w
+
+#--------------
+
+
+def getMainWindow():
+	'''returns the main window'''
+	toplevel = QtGui.qApp.topLevelWidgets()
+	for i in toplevel:
+		if i.metaObject().className() == "Gui::MainWindow":
+			return i
+	raise Exception("No main window found")
+
+
+def getComboView(mw):
+	'''returns the Combo View widget'''
+	dw = mw.findChildren(QtGui.QDockWidget)
+	for i in dw:
+		if str(i.objectName()) == "Combo View":
+			return i.findChild(QtGui.QTabWidget)
+		elif str(i.objectName()) == "Python Console":
+			return i.findChild(QtGui.QTabWidget)
+	raise Exception("No tab widget found")
+
+
+def ComboViewShowWidget(widget, tabMode=True):
+	'''create a tab widget inside the combo view'''
+
+	widget.tabname="Transportation Sketcher"
+	# stopp to default
+	if not tabMode:
+		widget.show()
+		return widget
+
+	mw = getMainWindow()
+	tab = getComboView(mw)
+
+	c = tab.count()
+
+	# clear the combo  window
+	for i in range(c - 1, 1, -1):
+		tab.removeTab(i)
+
+	# start the requested tab
+	tab.addTab(widget, widget.tabname)
+	tab.setCurrentIndex(2)
+
+	print "ComboViewShowWidget done."
+	widget.tab = tab
+	return widget
+
+
 
 
 
@@ -100,13 +226,36 @@ class _ViewProvider(nurbswb.pyob.ViewProvider):
 		print "my method B Starter"
 		# test starting an extra dialog
 		FreeCAD.d=dialog(self)
-		FreeCAD.d.show()
+		#FreeCAD.d.show()
 		FreeCAD.activeDocument().recompute()
 
 	def methodC(self,obj):
 		print "my method C After Edit finished"
+		
 		Gui.activateWorkbench("NurbsWorkbench")
+		print "kl"
+#		FreeCAD.d.hide()
+#		FreeCAD.d.deleteLater()
+		print "ha"
 		FreeCAD.activeDocument().recompute()
+		print "hu"
+		mw = getMainWindow()
+		tab = getComboView(mw)
+		c = tab.count()
+		print "count ",c
+		c = tab.count()
+
+		# clear the combo  window
+		for i in range(c - 1, 1, -1):
+			tab.removeTab(i)
+
+
+
+		tab.setCurrentIndex(0)
+		tab.setCurrentIndex(0)
+		print "set tab domne"
+
+
 
 	def unsetEdit(self,vobj,mode=0):
 		self.methodC(None)
@@ -256,7 +405,8 @@ def srun(w):
 	print "Run command:",cmd
 	eval(cmd)
 	Gui.SendMsgToActiveView("ViewFit")
-	# w.hide()
+	w.hide()
+	w.deleteLater()
 #\endcond
 
 
